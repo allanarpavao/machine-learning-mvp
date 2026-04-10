10cf75981c9a7a909483011cae3ced7676c7d589
allanapavao <allanarodrigues.p@gmail.com>
2026-04-10 16:52:32 -0300

    feat(api): adiciona classe pipeline

---
+++ api/models/pipeline.py
@@ -0,0 +1,15 @@
+import pickle
+
+class Pipeline:
+    
+    def __init__(self):
+        """Inicializa o pipeline"""
+        self.pipeline = None
+    
+    def carrega_pipeline(self, path):
+        """Carrega o pipeline construído
+        """
+        
+        with open(path, 'rb') as file:
+             self.pipeline = pickle.load(file)
+        return self.pipeline
